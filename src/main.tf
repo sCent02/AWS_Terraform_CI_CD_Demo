69ace4f2a2c0dfb79066a18b017d8969129bdba4
sCent02 <vince.52.va@gmail.com>
2025-09-21 22:58:09 +0800

    add files

--- src/main.tf
+++ src/main.tf
@@ -1,4 +1,14 @@
 terraform {
+    # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
+  # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
+  backend "s3" {
+    bucket         = "cc-tf-state-backend-ci-cd-vin"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+
     required_version = ">=0.13.0"
     required_providers {
         aws = {
@@ -10,4 +20,9 @@ terraform {
 
 provider "aws" {
     region = "ap-southeast-1"
+}
+
+module "tf-state" {
+    source = "./modules/tf-state"
+    bucket_name = "cc-tf-state-backend-ci-cd-vin"
 }
