16d316756a863680f56b4fe9d11277a5e4b5a9ea
sCent02 <vince.52.va@gmail.com>
2025-09-21 23:11:16 +0800

    added VPC and GH Actions

---
+++ src/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
+
+  vpc_cidr             = "10.0.0.0/16"
+  public_subnet_cidrs  = ["10.0.0.0/24", "10.0.1.0/24"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+}
